c0166a17ad24ab650e87688a32a9985e086c751b
wisdomgo <1246045735@qq.com>
2024-05-11 01:20:31 +0800

    🧪 test: 添加了用于测试红色行指令的汇编代码，测试通过

--- mipstestloop_sim.asm
+++ mipstestloop_sim.asm
@@ -1,30 +1,38 @@
-# Test the MIPS processor in simulation
-# add, sub, and, or, slt, addi, lw, sw, beq, j
-# If successful, it should write the value 7 to address 256, 260
+    .data
+    .text
+    .globl main
 
-#       Assembly                  Description           Instr   Address Machine
-main:   addi $2, $0, 5          # initialize $2 = 5     00      0       20020005
-        addi $3, $0, 12         # initialize $3 = 12    01      4       2003000c
-        addi $7, $3, -9         # initialize $7 = 3     02      8       2067fff7
-        or   $4, $7, $2         # $4 = (3 or 5) = 7     03      c       00e22025
-        and  $5, $3, $4         # $5 = (12 and 7) = 4   04      10      00642824
-        add  $5, $5, $4         # $5 = 4 + 7 = 11       05      14      00a42820
-        jalr $1, $3
-        beq  $5, $7, label2     # shouldn't be taken    06      18      10a7000a
-        slt  $4, $3, $4         # $4 = (12 < 7) = 0     07      1c      0064202a
-        beq  $4, $0, label1     # should be taken       08      20      10800001
-        addi $5, $0, 0          # shouldn't happen      09      24      20050000
-label1: slt  $4, $7, $2         # $4 = (3 < 5) = 1      0A      28      00e2202a
-        add  $7, $4, $5         # $7 = 1 + 11 = 12      0B      2c      00853820
-        sub  $7, $7, $2         # $7 = 12 - 5 = 7       0C      30      00e23822
-        sw   $7, 244($3)        # [256] = 7             0D      34      ac6700f4
-        lw   $2, 256($0)        # $2 = [256] = 7        0E      38      8c020100
-        j    label2             # should be taken       0F      3c      08000011
-        addi $2, $0, 1          # shouldn't happen      10      40      20020001
-label2: sw   $2, 260($0)        # write adr 260 = 7     11      44      ac020104
-loop:   j    loop               # dead loop             12      48      08000012
+main:
+    # 初始化寄存器值
+    lui   $t0, 0x1         # $t0 = 0x10000
+    ori   $t0, $t0, 0x1FF  # $t0 = 0x101FF
+    lui   $t1, 0x2         # $t1 = 0x20000
 
-# $0  = 0  # $1  = 0  # $2  = 7  # $3  = c
-# $4  = 1  # $5  = b  # $7  = 7
-# [0x100] = 7  [0x104] = 7
+    # 立即数逻辑与位操作
+    andi  $t2, $t0, 0xFF   # $t2 = $t0 AND 0xFF, $t2 = 0x1FF
+    slti  $t3, $t2, 0x200  # $t3 = $t2 < 0x200 (1为true)
+
+    # 移位操作
+    sll   $t4, $t3, 2      # $t4 = $t3 << 2
+    srl   $t5, $t4, 1      # $t5 = $t4 >> 1
+
+    # 变量移位
+    sllv  $t6, $t4, $t3    # $t6 = $t4 << $t3
+    srlv  $t7, $t6, $t3    # $t7 = $t6 >> $t3
+
+    # 逻辑操作
+    nor   $t8, $t6, $t7    # $t8 = ~($t6 | $t7)
+
+    # 分支和跳转
+    bne   $t7, $t8, skip   # 如果 $t7 != $t8，跳转到标签skip
+    nop                    # 延迟槽填充nop
+
+    # 跳转寄存器
+    jr    $ra              # 跳转回调用者
+    nop                    # 延迟槽填充nop
+
+skip:
+    # 跳转和链接寄存器
+    jalr  $ra, $t1         # 跳转到$t1地址，并且链接地址放入$ra
+    nop                    # 延迟槽填充nop
 
